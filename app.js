79ba78fb05bbc2553a6b0fba746d9ebfce338f5e
lucas_leandro <lucasrleandro@gmail.com>
2022-05-30 22:47:41 -0300

    cors fix

--- app.js
+++ app.js
@@ -1,42 +1,34 @@
-require("dotenv").config();
+require('dotenv').config();
 const cors = require('cors');
 
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 const caronaRoute = require('./routes/caronas-route');
 const userRoute = require('./routes/usuario-route');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+app.set(cors(), '*');
+
 app.use(bodyParser.json());
 app.use('/carona', caronaRoute);
 app.use('/usuario', userRoute);
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-      'Access-Control-Allow-Header',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-
-  if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).send({});
-  }
-  app.use(cors());
-  next();
-});
 
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
-mongoose.connect(
-    `mongodb+srv://${dbUser}:${dbPassword}@cluster0.tjoqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-    
+mongoose
+  .connect(
+    // `mongodb+srv://${dbUser}:${dbPassword}@cluster0.tjoqq.mongodb.net/?retryWrites=true&w=majority`
+    // 'mongodb://127.0.0.1:27017'
+    `mongodb+srv://root:root@cluster0.tjoqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
   )
   .then(() => {
-    console.log("Conectou ao banco!");
+    console.log('Conectou ao banco!');
     app.listen(3000);
   })
   .catch((err) => console.log(err));
